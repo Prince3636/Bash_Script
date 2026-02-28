a4e2186c59323999c761c9357381c2eac690674c
Prince <prince722663@gmail.com>
2026-02-28 06:46:05 +0000

    Add predefiened and user defined variables  examples

---
+++ basics/Variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Predefined variables
+echo "Hi"
+echo "Bash path: $BASH"
+echo "Bash version: $BASH_VERSION"
+echo "Home directory: $HOME"
+echo "Current working directory: $PWD"
+
+# User defined variables
+name="Prince"
+val=10
+
+echo "My name is: $name"
+echo "My value is: $val"
+
+echo "Script executed "
+exit 0
