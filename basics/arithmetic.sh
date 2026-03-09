65b7b6b787d96ffff05712cdd147a7e021e49496
Prince <raj722663@gmail.com>
2026-03-09 17:16:48 +0000

    update arithmetic repo

--- basics/arithmetic.sh
+++ basics/arithmetic.sh
@@ -11,3 +11,15 @@ echo "total sum is : $sum"
 echo "total diff is : $diff"
 echo "total mul is : $mul"
 echo "total div is : $div"
+
+
+# simple Counter
+
+echo  Enter number for printing counting 
+read -p "Printing the counting " num
+
+# for i in {1..5}
+for (( i = 1 ; i <=num ; i++ )) # also create like this
+do 
+   echo "Number : $i"
+done
